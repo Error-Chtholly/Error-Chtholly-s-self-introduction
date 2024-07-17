0015d3d3193d14ed3cb232ede5b4195a9bac294e
Error-Chtholly <1696304992@qq.com>
2024-07-17 22:40:03 +0800

    Error Chtholly上传了个人简历。

---
+++ script.js
@@ -0,0 +1,54 @@
+//这里存放js脚本
+function showGithub() { /* 这里定义showGithub函数 */
+    window.open("https://github.com/Error-Chtholly"); /* 这里调用window.open()方法打开github个人主页 */
+}
+function showWeibo() { /* 这里定义showWeibo函数 */
+    window.open("https://weibo.com/u/6069053271");
+}
+function showQQ() { /* 这里定义showQQ函数 */
+    alert("My QQ number : 1696304992."); /* 这里调用alert()方法弹出提示框 */
+    window.open("https://im.qq.com/");
+}
+function showWeixin() { /* 这里定义showWeixin函数 */
+    alert("My WeChat ID : zzt416993."); /* 这里调用alert()方法弹出提示框 */
+    window.open("https://wx.qq.com/");
+}
+function showContact() { /* 这里定义showContact函数 */
+    alert("Please contact me by email : 1696304992@qq.com or zzt15563168887@outlook.com"); /* 这里调用alert()方法弹出提示框 */
+}
+function learnHTML5() {
+    window.open("https://www.runoob.com/html/html5-intro.html");
+}
+function learnCSS3() {
+    window.open("https://www.runoob.com/css3/css3-tutorial.html");
+}
+function learnJS() {
+    window.open("https://www.runoob.com/js/js-tutorial.html");
+}
+function learnCAdd() {
+    window.open("https://www.runoob.com/cplusplus/cpp-tutorial.html");
+}
+function learnCS() {
+    window.open("https://www.runoob.com/csharp/csharp-tutorial.html");
+}
+function learnPy() {
+    window.open("https://www.runoob.com/python/python-tutorial.html");
+}
+function gotoErrorChtholly() {
+    var bgm = document.getElementById("bgm");
+    bgm.play();
+    window.open("https://baike.baidu.com/item/ATRI%20-My%20Dear%20Moments-/24229590"); // 这里调用window.open()方法打开百度百科ATRI - My Dear Moments-
+    window.open("https://baike.baidu.com/item/%E5%8F%AF%E5%A1%91%E6%80%A7%E8%AE%B0%E5%BF%86/15847619", "_blank"); // 这里调用window.open()方法打开新标签页
+}
+function contactlocation() {
+    window.open("https://www.ynnu.edu.cn/");
+}
+function contactphone() {
+    alert("My phone number is 15563168887 or 19819056097.");
+}
+function contactemail() {
+    alert("My email address is 1696304992@qq.com or zzt15563168887@outlook.com.");
+}
+function rights() {
+    window.open("https://error-chtholly.github.io/Error-Chtholly-s-self-introduction/");
+}
